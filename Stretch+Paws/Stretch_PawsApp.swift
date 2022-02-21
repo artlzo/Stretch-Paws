4ba472de468b621d64ec08fab7e2db30a5efcc76
Arturo Liranzo <arturolzo@gmail.com>
2022-02-21 09:44:17 -0500

    Initial Commit

---
+++ Stretch+Paws/Stretch_PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stretch_PawsApp.swift
+//  Stretch+Paws
+//
+//  Created by Arturo Liranzo on 2/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Stretch_PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
