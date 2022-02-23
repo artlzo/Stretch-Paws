33500161e5d463ee6e887cae9c166c51e41f6fdc
Arturo Liranzo <arturolzo@gmail.com>
2022-02-23 08:29:06 -0500

    Added the poses data

--- Stretch+Paws/ContentView.swift
+++ Stretch+Paws/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                NavigationLink(destination: DetailView()) {
+                    Text("Downward-facing Dog")
+                }
+                
+                Text("Standing Forward Fold")
+                Text("Tree Pose")
+            }
+            .listStyle(.grouped)
+            .navigationTitle("Stretch + Paws")
+        }
+        
+        
     }
-}
 
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
---
+++ Stretch+Paws/DetailView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailView.swift
+//  Stretch+Paws
+//
+//  Created by Arturo Liranzo on 2/21/22.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("Purrrrfect!")
+            .font(.title)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
