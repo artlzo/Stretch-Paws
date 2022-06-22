3ecbe6efe064609dd35a01d45e6c2ad184d6c3e0
Arturo Liranzo <arturolzo@gmail.com>
2022-06-22 10:28:51 -0400

    New setup

--- Stretch+Paws/DetailView.swift
+++ Stretch+Paws/DetailView.swift
@@ -51,6 +51,7 @@ struct DetailView: View {
                     
                 }.padding(.horizontal, 20)
             }
+            TimerPanelView()
         }
     }
 }
@@ -59,5 +60,27 @@ struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
         DetailView(pose: Pose(
             name: "Downward-Facing Dog", asana: "Adho Mukha Shvanasana", icon: "Cat-1", description: "Did someone say dog, Can't we call this a downward-facing cat instead? It's OK - This is a friendly  dog, its not intereseted in chasing cats. In fact Downward-Facing Dog is the lynchpin of a yoga asana practice: if you're going to befriend with any of these poses, make sure it's this canine classic.", steps: ["From a kneeling position, place your hands shoulde-distance apart and spread your finders", "Tuck your toes and lift your hips up towards the celining so you can create an inverted V shape.", "Balance the weight between hands and feet and think about tilting your tailbone up towards the ceiling.", "Send your gaze towards your feet and breath!"], topTip: "Bend your knees in order to create more length through the spine") )
+    }
+}
+
+struct TimerPanelView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            Text("try it out")
+            .padding(40)
+            .foregroundColor(Color("Secondary"))
+            .frame(maxWidth: .infinity, maxHeight: 80)
+            .background(Color("Highlight"))
+            .cornerRadius(6)
+            
+        } .ignoresSafeArea()
+    }
+}
+
+
+struct TimerOpenView: View {
+    var body: some View {
+        Text("hold that pose ")
     }
 }
